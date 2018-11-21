148cb3b55ffdd6569f603af09e5f3fcd07d8861a
Shaun Densley <twixx1987@gmail.com>
2018-11-20 21:19:25 -0800

    updated files

--- index.js
+++ index.js
@@ -1,46 +1,30 @@
 const express = require('express');
-var parser = require('body-parser');
 const path = require('path');
-const rateCalc = require('./calculateRate');
+const { Client } = require('pg');
 const PORT = process.env.PORT || 5000;
 
+const connectionString = process.env.DATABASE_URL ||
+    'postgres://iaaozceadeifaz:117a683a2caa80bae52baab6b9a690894ad5f023a986622cabf55d8de5420e7e@ec2-54-83-8-246.compute-1.amazonaws.com:5432/d622jstp7u5qmp';
+
+const db = new Client({
+    connectionString: connectionString,
+    ssl: true
+});
+
 express()
-    .use(parser.urlencoded({ extended: false }))
-    .use(parser.json())
     .use(express.static(path.join(__dirname, 'public')))
     .set('views', path.join(__dirname, 'views'))
     .set('view engine', 'ejs')
     .get('/', (req, res) => res.render('pages/index'))
-    .get('/mathForm', (req, res) => res.render('pages/mathForm'))
-    .get('/rateCalc', (req, res) => res.render('pages/postalRateCalculator', {
-        type: null,
-        weight: null,
-        rate: null
-    }))
-    .post('/math', (req, res) => {
-            let equation = req.body.lhs + req.body.operator + req.body.rhs;
+    .get('/getPerson', (req, res) => {
+        const { id } = req.query;
 
-            let mathResult = eval(equation);
-
-            res.render('pages/result', {
-                result: mathResult
-            })
-        })
-    .post('/math_service', (req, res) => {
-        let equation = req.body.lhs + req.body.operator + req.body.rhs;
-
-        let mathResult = eval(equation);
-
-        res.json({ result: mathResult })
-    })
-    .post('/rateCalc', (req, res) => {
-        let type = req.body.type;
-        let weight = req.body.weight;
-        let rate = rateCalc(type, weight);
-        res.render('pages/postalRateCalculator', {
-            type: type,
-            weight : weight,
-            rate: rate
+        db.connect().then(() => {
+            db.query(`SELECT * FROM person WHERE id = ${id}`).then(result => {
+                res.setHeader('Content-Type', 'application/json');
+                res.send(JSON.stringify(result.rows[0]));
+                db.end();
             });
+        });
     })
     .listen(PORT, () => console.log(`Listening on ${PORT}`));
